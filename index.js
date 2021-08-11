553171dce3110930f5d3cd97672101f58d8c7028
7louisjeon <wjss5115@gmail.com>
2021-08-12 08:59:42 +0900

    AE

--- index.js
+++ index.js
@@ -53,9 +53,12 @@ app.listen(process.env.PORT || 5000, () => {
   console.log("Backend is running.");
 });
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get("*", (req, res) => {
-  let url = path.join(__dirname, "build", "index.html");
+  let url = path.join(__dirname, "../client/build", "index.html");
+  if (!url.startsWith("/app/")) {
+    url = url.substring(1);
+  }
   res.sendFile(url);
 });
