254be89b87e633539f189a04ee6aded8b20a067f
7louisjeon <wjss5115@gmail.com>
2021-08-12 09:27:32 +0900

    tes

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ const path = require("path");
 
 dotenv.config();
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
 app.use(express.json());
