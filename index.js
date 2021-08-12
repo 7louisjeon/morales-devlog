9aa6c2668d03f7211b96f5f2be26c11540b14d5a
7louisjeon <wjss5115@gmail.com>
2021-08-13 02:04:18 +0900

    CORS update

--- index.js
+++ index.js
@@ -16,8 +16,9 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, POST, PUT, GET, DELETE, OPTIONS"
+    "Origin, X-Requested-With, Content-Type, Accept, POST, PUT, GET, DELETE, OPTIONS, Access-Control-Allow-Headers, Authorization"
   );
+  res.header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
   next();
 });
 
